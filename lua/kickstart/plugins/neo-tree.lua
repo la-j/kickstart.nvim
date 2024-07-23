13c941234e8e04d06cc487e89c6ebab43bcb8a74
John Joseph Ugalino <john@lawadvisor.com>
2024-07-23 17:41:49 +0800

    Use unicode glyphs for icons

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -21,6 +21,58 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      components = {
+        icon = function(config, node, state)
+          local highlights = require 'neo-tree.ui.highlights'
+
+          local icon = config.default or ' '
+          local padding = config.padding or ' '
+          local highlight = config.highlight or highlights.FILE_ICON
+
+          if node.type == 'directory' then
+            highlight = highlights.DIRECTORY_ICON
+            if node:is_expanded() then
+              icon = config.folder_open or '-'
+            else
+              icon = config.folder_closed or '+'
+            end
+          elseif node.type == 'file' then
+            icon = 'F'
+          end
+
+          return {
+            text = icon .. padding,
+            highlight = highlight,
+          }
+        end,
+      },
+    },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added = '+',
+          deleted = '-',
+          modified = '~',
+          renamed = 'R',
+          -- Status type
+          untracked = 'UT',
+          ignored = 'I',
+          unstaged = 'US',
+          staged = 'S',
+          conflict = 'C',
+        },
+      },
+      icon = {
+        folder_closed = '+',
+        folder_open = '-',
+        folder_empty = '.',
+        folder_empty_open = 'X',
+        -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+        -- then these will never be used.
+        default = '*',
+        highlight = 'NeoTreeFileIcon',
+      },
     },
   },
 }
