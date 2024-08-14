9a5c37956c7b7def1d984aeae336871e82a66ce4
John Joseph Ugalino <john@lawadvisor.com>
2024-08-14 16:00:39 +0800

    Update conform setup

--- init.lua
+++ init.lua
@@ -651,7 +651,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
