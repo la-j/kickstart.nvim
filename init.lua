4e042fad67e3161601a0797d36dd5fe9f336b50c
John Joseph Ugalino <john@lawadvisor.com>
2024-09-09 19:45:09 +0800

    Change ts lsp

--- init.lua
+++ init.lua
@@ -561,7 +561,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
 
         -- elixirls = {},
         lexical = {
